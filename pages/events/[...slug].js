7e7c96c3321ecfbae8363876abf61e068c4654c8
Tingting Wei <twei3@academic.rrc.ca>
2024-05-10 08:51:06 -0500

    add pages

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+function EventPostPage() {
+  return (
+    <div>
+      <h1>
+        The Event Post Page
+      </h1>
+    </div>
+  );
+}
+
+export default EventPostPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function AllEventsPage() {
+  return (
+    <div>
+      <h1>The Event Page Shows All Events</h1>
+    </div>
+  );
+}
+
+export default AllEventsPage;
